0e08d129917d710997e2ebd02e4b4f8645d1d3d0
cin1388a <a.galasso@cineca.it>
2014-07-20 21:00:05 +0200

    del

--- caspacservernew/src/main/resources/security/resetIdpMeta.bat
+++
@@ -1,6 +0,0 @@
-cd C:\shibbolethtest\shiboleth_install_output\logs
-del *.*
-cd C:\shibbolethtest\shiboleth_install_output\metadata
-del casalessandroit_spNew.xml
-cd C:\shibbolethtest\STSworkspace\cas-pac4j-spring-security-saml-demo\src\main\resources\security
-copy casalessandroit_spNew.xml  C:\shibbolethtest\shiboleth_install_output\metadata
